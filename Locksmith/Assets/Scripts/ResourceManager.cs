ced680223767800a7419da7c17f7948f02b1795d
taarruzkecisi <furkancylan@gmail.com>
2021-06-05 12:59:07 +0300

    feat(resource types)

---
+++ Locksmith/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance { get; private set; }
+
+    private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
+
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+    }
+
+}
