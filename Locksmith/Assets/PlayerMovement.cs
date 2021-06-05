6d880ff7b1d15346d112955aff3c6a3d4f690a1a
taarruzkecisi <furkancylan@gmail.com>
2021-06-05 12:05:23 +0300

    feat(player movement)

---
+++ Locksmith/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 moveDirection;
+    [SerializeField] private float moveSpeed = 50f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection * (moveSpeed * Time.fixedDeltaTime);
+    }
+    private void MovementInput()
+    {
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection.normalized;
+    }
+
+
+}
