02aabf0b2650d1ec7cae890a7f56e689d4b0ea9a
taarruzkecisi <furkancylan@gmail.com>
2021-06-05 13:08:15 +0300

    feat(basic add resource)

--- Locksmith/Assets/Scripts/ResourceManager.cs
+++ Locksmith/Assets/Scripts/ResourceManager.cs
@@ -15,6 +15,32 @@ public class ResourceManager : MonoBehaviour
         resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
 
         ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            resourceAmountDictionary[resourceType] = 0;
+        }
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+            AddResource(resourceTypeList.list[0], 2);
+            TestLogResourceAmountDictionary();
+        }
+    }
+
+    private void TestLogResourceAmountDictionary()
+    {
+        foreach(ResourceTypeSO resourceType in resourceAmountDictionary.Keys)
+        {
+            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]);
+        }
+    }
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        resourceAmountDictionary[resourceType] += amount;
+    }
 }
