3b44d47298902dcd3005138a040ff20fffc66eca
Gökcan Kırca <gokcankrc@gmail.com>
2021-06-20 18:58:24 +0300

    A shitty healthbar has been added, prefabs changed

---
+++ Locksmith/Assets/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Transform healthTransform;
+    private HealthBaseClass healthClass;
+    private int healthMax => healthClass.maxHealth;
+    private float health => healthClass.health;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        healthClass = GetComponentInParent<HealthBaseClass>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // might wanna lossy scale this later
+        /*
+        var lossyScale = healthTransform.lossyScale;
+        var xSize = health / healthMax;
+        xSize = xSize / lossyScale.x;
+        var ySize = 1 / lossyScale.x;
+        */
+        var xSize = health / healthMax;
+        healthTransform.localScale = new Vector2(xSize, 1);
+    }
+}
